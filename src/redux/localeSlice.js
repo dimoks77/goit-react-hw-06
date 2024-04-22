38f5bd4c6f45c18139d6ba7119444f58e979d5be
Dmytro Kasko <dimoks77@gmail.com>
2024-04-23 00:12:05 +0200

    npm intall @reduxjs/toolkit

---
+++ src/redux/localeSlice.js
@@ -0,0 +1,10 @@
+const localInitialState = {
+    lang: 'uk',
+};
+
+export const localeReducer = (state = localInitialState, action) => {
+    switch (action.type) {
+        case: 'locale/changeLang':
+            return {}
+    }
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,10 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { balanceReducer } from './balanceSlice';
+import { localReducer } from './localeSlice';
 
-
-export const deposit = value => {
-    return {
-    type: 'balance/deposit',
-    payload: value,
+export const store = configureStore({
+    reducer: {
+        balance: balanceReducer,
+        locale: localReducer,
     }
-}
-
-const initialState = {
-    balance: {
-        value: 0,
-    },
-};
-
-const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'balance/deposit':
-            return {
-                balance: {
-                    value: state.balance.value + action.payload,
-                },
-            };
-    default:
-    return state;
-    }
-};
-
-export const store = createStore(rootReducer, devToolsEnhancer());
+});
