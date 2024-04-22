518d996d9718c7833b65f5c1da8c88a883a42ea8
Dmytro Kasko <dimoks77@gmail.com>
2024-04-23 00:02:33 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { deposit } from '../redux/store';
 
-function App() {
+export default function App() {
+  const dispatch = useDispatch();
+  const balance = useSelector(state => state.balance.value);
+  console.log(balance);
 
   return (
     <>
-      test
+      <p>Balance: {balance}</p>
+      <button onClick={() => dispatch(deposit(10))}>Deposit 10 credits</button>
+      <button onClick={() => dispatch(deposit(5))}>Deposit 5 credits</button>
     </>
   )
 }
 
-export default App
+// export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,31 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
-// const rootReducer = (state, action) => {
-//     return state;
-// }
 
-export const store = createStore();
+export const deposit = value => {
+    return {
+    type: 'balance/deposit',
+    payload: value,
+    }
+}
+
+const initialState = {
+    balance: {
+        value: 0,
+    },
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'balance/deposit':
+            return {
+                balance: {
+                    value: state.balance.value + action.payload,
+                },
+            };
+    default:
+    return state;
+    }
+};
+
+export const store = createStore(rootReducer, devToolsEnhancer());
