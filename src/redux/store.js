6948ed314ce66b74fb974578b2d92ed720a13cf2
Dmytro Kasko <dimoks77@gmail.com>
2024-04-24 12:53:46 +0200

    the end - 3

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,14 @@ import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
 import { contactReducer } from './contactsSlice';
 import { filterReducer } from './filtersSlice';
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
 
 const rootReducer = combineReducers({
   contacts: contactReducer,
@@ -20,11 +28,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
